7fc8f01b7f469e3dd78e025fbe494faaa71dadb6
Jackson <themarduk28@gmail.com>
2018-08-29 10:07:49 -0400

    Updated WIPscript to best and added note to changelist

--- WIPexperimental.bat
+++ WIPexperimental.bat
@@ -1,28 +1,47 @@
 @echo off
 
+:: Set variables
+set desktop=%userprofile%\Desktop
+set compfiles=%desktop%\Win7CompFiles
+set pshellrun=@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command
+
 :: Install git and download folders
 cls
 echo Installing git and downloading folders...
 echo.
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+%pshellrun% "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco feature enable -n allowGlobalConfirmation
 choco install git
-cd %userprofile%\Desktop
+cd %desktop%
 git init
 git remote add origin https://github.com/Marduk28/CyberPatriot_Windows_Scripts.git
 git fetch origin master
 git checkout origin/master Win7CompFiles OurGloriousChecklist2018_Windows.txt
+echo.
+echo Done!
+echo.
+pause
 
 :: Apply SCM Baselines
 cls
 echo Applying SCM Baselines...
-"%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\LGPO.exe" /g "%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\Win7\Computer_Sec"
-"%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\LGPO.exe" /g "%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\Win7\Domain_Sec"
-"%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\LGPO.exe" /g "%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\Win7\User_Sec"
-"%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\LGPO.exe" /g "%USERPROFILE%\Desktop\Win7CompFiles\SCMBaselines\Win7\BitLocker_Sec"
+"%compfiles%\SCMBaselines\LGPO.exe" /g "%compfiles%\SCMBaselines\Win7\Computer_Sec"
+"%compfiles%\SCMBaselines\LGPO.exe" /g "%compfiles%\SCMBaselines\Win7\Domain_Sec"
+"%compfiles%\SCMBaselines\LGPO.exe" /g "%compfiles%\SCMBaselines\Win7\User_Sec"
+"%compfiles%\SCMBaselines\LGPO.exe" /g "%compfiles%\SCMBaselines\Win7\BitLocker_Sec"
+echo.
+echo Done!
+echo.
+pause
 
 :: Generate user list
 cls
 echo Generating user list...
 echo.
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Get-LocalUser > userlist.txt"
+%pshellrun% "Get-LocalUser > users.txt"
+for /f "skip=3 tokens=1 usebackq" %%G in ("users.txt") do `echo %%G > %desktop%\userlist.txt`
+del /f /q users.txt
+echo.
+echo Done!
+echo.
+pause
